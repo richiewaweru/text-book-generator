7af058cedd7ef783f83761c68426f8b48d12108e
richiewaweru <richie@uni.minerva.edu>
2026-04-03 08:16:07 -0400

    fix: remove adapter-static options for Vercel zero-config mode

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -3,9 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			fallback: 'index.html'
-		})
+		adapter: adapter()
 	}
 };
 
